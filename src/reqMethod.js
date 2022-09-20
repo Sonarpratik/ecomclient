f4d887ac88c26d47cd5f7c68ddbb4e50eba99ab8
Sonarpratik <sonarpratik02.ps@gmail.com>
2022-09-20 21:02:24 +0530

    added new api

--- src/reqMethod.js
+++ src/reqMethod.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = `http://localhost:5000/api/`;
+// const BASE_URL = `http://localhost:5000/api/`;
+const BASE_URL = `https://ecombackend.sonarpratik.repl.co/api/`;
 const Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMTIzNTA4YzdmOWFlZTExMjUxOWQyMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjcyMjE2NSwiZXhwIjoxNjYyOTgxMzY1fQ.ktOIFeJ96bQYEGMgviBLNTdlE8v5vOvym6LUJ7fd8X8`;
 
 export const publicRequest = axios.create({
